251a1ad92622f9cc99eee00816090cf789a38ce0
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-09 14:12:47 +0200

    abruptlyGoblins_step2

--- Abruptly Goblins/script.py
+++ Abruptly Goblins/script.py
@@ -4,3 +4,14 @@ def add_gamer(name, gamers_list):
         gamers_list.append(gamers)
     else:
         print("Gamer missing critical information")
+
+add_gamer({'name':"Kimberly Warner",'availability':["Monday", "Tuesday",  "Friday"]}, gamers)
+add_gamer({'name':'Thomas Nelson','availability': ["Tuesday", "Thursday", "Saturday"]}, gamers)
+add_gamer({'name':'Joyce Sellers','availability': ["Monday", "Wednesday", "Friday", "Saturday"]}, gamers)
+add_gamer({'name':'Michelle Reyes','availability': ["Wednesday", "Thursday", "Sunday"]}, gamers)
+add_gamer({'name':'Stephen Adams','availability': ["Thursday", "Saturday"]}, gamers)
+add_gamer({'name': 'Joanne Lynn', 'availability': ["Monday", "Thursday"]}, gamers)
+add_gamer({'name':'Latasha Bryan','availability': ["Monday", "Sunday"]}, gamers)
+add_gamer({'name':'Crystal Brewer','availability': ["Thursday", "Friday", "Saturday"]}, gamers)
+add_gamer({'name':'James Barnes Jr.','availability': ["Tuesday", "Wednesday", "Thursday", "Sunday"]}, gamers)
+add_gamer({'name':'Michel Trujillo','availability': ["Monday", "Tuesday", "Wednesday"]}, gamers)
