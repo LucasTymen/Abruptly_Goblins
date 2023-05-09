b275661420732d27c9002ffb4fb52a94fae5143b
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-09 14:44:54 +0200

    abruptlyGoblins_step5

--- Abruptly Goblins/script.py
+++ Abruptly Goblins/script.py
@@ -18,6 +18,15 @@ add_gamer({'name':'James Barnes Jr.','availability': ["Tuesday", "Wednesday", "T
 add_gamer({'name':'Michel Trujillo','availability': ["Monday", "Tuesday", "Wednesday"]}, gamers)
 
 def build_daily_frequency_table():
-    return {'availability': 0}
+    return {"Monday": 0,
+            "Tuesday" : 0,
+            "Wednesday" : 0,
+            "Thursday" : 0,
+            "Friday" : 0,
+            "Saturday" : 0,
+            "Sunday" : 0,
+            }
 
 count_availability = build_daily_frequency_table
+
+def calculate_availability(gamers_list, available_frequency)
